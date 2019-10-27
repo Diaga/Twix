040d713a4e3cd7a69e30c85710204bb99ab3ea42
Maaz Bin Tahir <diagaaos@gmail.com>
2019-10-27 13:46:10 +0500

    Fixed task done and delete logic for assigned tasks

--- lib/Widgets/build_task_card.dart
+++ lib/Widgets/build_task_card.dart
@@ -5,32 +5,30 @@ import 'package:twix/Database/database.dart';
 import 'package:twix/Widgets/task_card.dart';
 import 'onswipe_container.dart';
 
-
-
 class BuildTaskCard extends StatefulWidget {
   final TaskWithBoard taskItem;
   final AssignedTaskWithUser assignedTaskItem;
   final TwixDB database;
 
-
-  BuildTaskCard({this.taskItem , this.assignedTaskItem , this.database});
+  BuildTaskCard({this.taskItem, this.assignedTaskItem, this.database});
 
   @override
   _BuildTaskCardState createState() => _BuildTaskCardState();
 }
 
-class _BuildTaskCardState extends State<BuildTaskCard> with SingleTickerProviderStateMixin{
-
+class _BuildTaskCardState extends State<BuildTaskCard>
+    with SingleTickerProviderStateMixin {
   AnimationController _animationController;
   Animation<double> _animation;
 
-
   @override
   void initState() {
-   super.initState();
-   _animationController = AnimationController(vsync: this,duration: Duration(milliseconds: 800));
-   _animation = CurvedAnimation(parent: _animationController,curve: Curves.ease);
-   _animationController.forward();
+    super.initState();
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 800));
+    _animation =
+        CurvedAnimation(parent: _animationController, curve: Curves.ease);
+    _animationController.forward();
   }
 
   @override
@@ -43,57 +41,66 @@ class _BuildTaskCardState extends State<BuildTaskCard> with SingleTickerProvider
   Widget build(BuildContext context) {
     final TaskCard taskCard = TaskCard(
       task: widget.taskItem != null ? widget.taskItem : null,
-      assignedTask: widget.assignedTaskItem != null ? widget.assignedTaskItem : null,
+      assignedTask:
+          widget.assignedTaskItem != null ? widget.assignedTaskItem : null,
     );
-    bool isDone =
-    widget.taskItem == null ? widget.assignedTaskItem.task.isDone : widget.taskItem.task.isDone;
+    bool isDone = widget.taskItem == null
+        ? widget.assignedTaskItem.task.isDone
+        : widget.taskItem.task.isDone;
     DismissDirection dismissDirection =
-    isDone ? DismissDirection.endToStart : DismissDirection.horizontal;
+        isDone ? DismissDirection.endToStart : DismissDirection.horizontal;
     return FadeTransition(
       opacity: _animation,
       child: Builder(
-          builder: (context) =>
-              Dismissible(
-                key: ValueKey(taskCard.hashCode),
-                direction: dismissDirection,
-                background: OnSwipeContainer(
-                  color: Colors.blue,
-                  iconData: Icons.check,
-                  alignment: Alignment.centerLeft,
-                ),
-                child: taskCard,
-                onDismissed: (DismissDirection direction) {
-                  if (direction == DismissDirection.startToEnd) {
-                    // Logic to update the task to isDone
-                    widget.database.taskDao
-                        .updateTask(widget.taskItem.task.copyWith(isDone: true));
+        builder: (context) => Dismissible(
+          key: ValueKey(taskCard.hashCode),
+          direction: dismissDirection,
+          background: OnSwipeContainer(
+            color: Colors.blue,
+            iconData: Icons.check,
+            alignment: Alignment.centerLeft,
+          ),
+          child: taskCard,
+          onDismissed: (DismissDirection direction) {
+            if (direction == DismissDirection.startToEnd) {
+              // Logic to update the task to isDone
+              widget.assignedTaskItem == null
+                  ? widget.database.taskDao
+                      .updateTask(widget.taskItem.task.copyWith(isDone: true))
+                  : widget.database.taskDao.updateTask(
+                      widget.assignedTaskItem.task.copyWith(isDone: true));
 
-                    // Display snack bar
-                    Scaffold.of(context).showSnackBar(
-                      SnackBar(
-                        content: Text("Done"),
-                        duration: Duration(milliseconds: 600),
-                      ),
-                    );
-                  } else if (direction == DismissDirection.endToStart) {
-                    // Logic to delete the task
-                    widget.database.taskDao.deleteTask(widget.taskItem.task);
-
-                    // Display snack bar
-                    Scaffold.of(context).showSnackBar(
-                      SnackBar(
-                        content: Text("Deleted"),
-                        duration: Duration(milliseconds: 600),
-                      ),
-                    );
-                  }
-                },
-                secondaryBackground: OnSwipeContainer(
-                  color: Colors.red,
-                  iconData: Icons.delete,
-                  alignment: Alignment.centerRight,
+              // Display snack bar
+              Scaffold.of(context).showSnackBar(
+                SnackBar(
+                  content: Text("Done"),
+                  duration: Duration(milliseconds: 600),
                 ),
-              ),),
-    );;
+              );
+            } else if (direction == DismissDirection.endToStart) {
+              // Logic to delete the task
+              widget.assignedTaskItem == null
+                  ? widget.database.taskDao.deleteTask(widget.taskItem.task)
+                  : widget.database.assignedTaskDao
+                      .deleteAssignedTask(widget.assignedTaskItem.assignedTask);
+
+              // Display snack bar
+              Scaffold.of(context).showSnackBar(
+                SnackBar(
+                  content: Text("Deleted"),
+                  duration: Duration(milliseconds: 600),
+                ),
+              );
+            }
+          },
+          secondaryBackground: OnSwipeContainer(
+            color: Colors.red,
+            iconData: Icons.delete,
+            alignment: Alignment.centerRight,
+          ),
+        ),
+      ),
+    );
+    ;
   }
 }
